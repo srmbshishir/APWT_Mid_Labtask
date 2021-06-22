eb98f9cdddbbbf76342c8420929bc639f8b624f7
srmbshishir <srmb.shishir@gmail.com>
2021-06-22 14:12:12 +0600

    Login feature M1F1

---
+++ midlabtask/resources/views/Login/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	<h1>Login Page</h1>
+
+	<form method="post">
+        <table>
+            <tr>
+                <td>Email</td>
+                <td><input type="text" name="email"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" name="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="submit" value="Submit"></td>
+                <td></td>
+            </tr>
+        </table>
+	</form>
+</body>
+</html>
